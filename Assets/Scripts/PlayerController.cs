401ae374951efed43955e2b610396aee950c7d5c
GorillaOne <biz@gorillatactics.com>
2020-11-07 13:28:04 -0500

    Added basic player prefab and ability to control it.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float m_speed; 
+	private Rigidbody2D m_rigidbody2D;
+
+	private void Awake()
+	{
+		m_rigidbody2D = GetComponent<Rigidbody2D>(); 
+	}
+
+	public void Move(Vector2 move)
+	{
+		m_rigidbody2D.velocity = move * m_speed; 
+	}
+}
